ca0b118f08c2597239bd571170a7b95f5a52f5b2
Dan de Havilland <dan@dandehavilland.com>
2020-04-27 14:38:02 +0200

    First pass at responsive header

--- source/js/_scripts.js
+++ source/js/_scripts.js
@@ -12,13 +12,6 @@ function getUrlVars(){
 
 $(document).ready(function() {
 
-  // Nav menu interactions
-  $('#show_hide_nav').click(function() {
-    $('#site_header').toggleClass('is-active');
-    $('section').toggleClass('is-slid');
-    $(this).toggleClass('is-active');
-  });
-
   $('section, footer').click(function() {
     $('#site_header').removeClass('is-active');
     $('section').removeClass('is-slid');
@@ -44,41 +37,6 @@ $(document).ready(function() {
   $('#slider-4').click(function() {
     $('#slide-4').removeClass('is-inactive');
   });
-
-  $(window).scroll(updateHeaderTransparency);
-
-  function updateHeaderTransparency(){
-    let opaqueTop = window.scrollY > 30;
-    $('.fixed-top').toggleClass('opaque', opaqueTop);
-  }
-  setTimeout(updateHeaderTransparency, 300);
-  //set up contact form
-  // if($('#contact_form').size() > 0){
-  //   $("#contact_form").validate();
-  //   $("#contact_form").submit(function(e){
-  //     if($(this).valid()){
-  //       // get data
-  //       var post_data = $(this).serializeArray();
-  //       var form_url = $(this).attr("action");
-  //       var form_method = $(this).attr("method");
-
-  //       $.ajax({
-  //           url: form_url,
-  //           method: form_method,
-  //           data: post_data,
-  //           dataType: "json",
-  //           success: function(res){
-  //             if(res && res.success){
-  //               $('.js-before-send').addClass('is-hidden');
-  //               $('.js-after-send').removeClass('is-hidden');
-  //             }
-  //           }
-  //       });
-  //     }
-  //     e.preventDefault(); //STOP default action
-  //   });
-  // }
-
 
   // grab the UTM details and put it in a cookie
   var urlVars = getUrlVars();
